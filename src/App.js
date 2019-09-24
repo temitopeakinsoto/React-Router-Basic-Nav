e14b659af2798a0fded1306725b65c893c223633
Adetunji Awobajo <surfsamson@gmail.com>
2019-09-24 01:30:39 -0700

    Declare routes for project
    functionality is then used to create three different routes to '/', '/about'
    and '/contact' components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path='/' component={} exact />
+    <Route path='about' component={} />
+    <Route path='contact' component={} />
   </div>
 );
 
